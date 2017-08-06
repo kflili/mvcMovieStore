2b1b59f31b3f54d42120e2abc63a3e9758f4e4c1
LiLi <kflili@gmail.com>
2017-08-05 21:49:25 -0400

    Add first M-V-C

---
+++ mvcMovieStore/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using mvcMovieStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcMovieStore.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            return View(movie);
+        }
+    }
+}
