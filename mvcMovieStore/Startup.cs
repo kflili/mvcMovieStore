9c9619f60f2974aa162bed12686202c221bd88e2
LiLi <kflili@gmail.com>
2017-08-05 21:43:16 -0400

    Add project files.

---
+++ mvcMovieStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvcMovieStore.Startup))]
+namespace mvcMovieStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
