f1ab3e48c1452afef82808a08003692142a1c067
LiLi <kflili@gmail.com>
2017-08-05 23:57:07 -0400

    Ex1 solution b, Add Movies Index and detail

--- mvcMovieStore/Controllers/MoviesController.cs
+++ mvcMovieStore/Controllers/MoviesController.cs
@@ -10,6 +10,34 @@ namespace mvcMovieStore.Controllers
 {
     public class MoviesController : Controller
     {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        // GET: Movies/Details/2
+        public ActionResult Details(int id)
+        {
+            var movie = GetMovies().SingleOrDefault(m => m.ID == id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { ID = 1, Name = "Shrek!" },
+                new Movie { ID = 2, Name = "Wall-e"}
+            };
+        }
+
         // GET: Movies/Random
         public ActionResult Random()
         {
@@ -19,16 +47,12 @@ namespace mvcMovieStore.Controllers
                 new Customer{Name = "Customer 1"},
                 new Customer{Name = "Customer 2"}
             };
-
             var viewModel = new RandomMovieViewModel
             {
                 Movie = movie,
                 Customers = customers
             };
-
             return View(viewModel);
         }
-
-        
     }
 }
