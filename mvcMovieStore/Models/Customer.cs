d45d68972a7c704eb555c92c75b30c7cf150e731
LiLi <kflili@gmail.com>
2017-08-08 17:45:15 -0400

    enable code first and intial model

--- mvcMovieStore/Models/Customer.cs
+++ mvcMovieStore/Models/Customer.cs
@@ -9,5 +9,8 @@ namespace mvcMovieStore.Models
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public int MembershipTypeID { get; set; }
+        public MembershipType MembershipType { get; set; }
     }
 }
