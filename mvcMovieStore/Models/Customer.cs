d5f26b89430965dc21f388e86b9779d45c86221f
LiLi <kflili@gmail.com>
2017-08-08 17:58:08 -0400

    ApplyAnnotationsToCustomerName

--- mvcMovieStore/Models/Customer.cs
+++ mvcMovieStore/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,8 @@ namespace mvcMovieStore.Models
     public class Customer
     {
         public int ID { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public int MembershipTypeID { get; set; }
