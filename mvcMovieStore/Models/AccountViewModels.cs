f9d00eff7de0e32b178133f11157364bfe1b8b74
LiLi <kflili@gmail.com>
2017-08-11 15:42:06 -0400

    Section 8 Exercise - Add phone to application user.

--- mvcMovieStore/Models/AccountViewModels.cs
+++ mvcMovieStore/Models/AccountViewModels.cs
@@ -12,6 +12,10 @@ namespace mvcMovieStore.Models
         [Required]
         [Display(Name = "Driving License")]
         public string DrivingLicense { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Phone { get; set; }
     }
 
     public class ExternalLoginListViewModel
@@ -72,6 +76,10 @@ namespace mvcMovieStore.Models
         [Display(Name = "Driving License")]
         [StringLength(255)]
         public string DrivingLicense { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Phone { get; set; }
 
         [Required]
         [EmailAddress]
